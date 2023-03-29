1173589fb1f8a11c105b6de8572c669a6564edf6
Tianqing Liu <liutq@hollycrm.com>
2023-03-29 15:43:31 +0800

    通过Spring配置数据源池

--- src/test/java/top/smartliu/dataPool/DataSourceTest.java
+++ src/test/java/top/smartliu/dataPool/DataSourceTest.java
@@ -3,10 +3,26 @@ package top.smartliu.dataPool;
 import com.alibaba.druid.pool.DruidDataSource;
 import com.alibaba.druid.pool.DruidPooledConnection;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import javax.sql.DataSource;
 
+import java.sql.Connection;
 import java.sql.SQLException;
 
 public class DataSourceTest {
+    @Test
+    public void testSpringDruid() throws SQLException {
+        ClassPathXmlApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
+        // 支持使用类名获取 bean
+        // DataSource dataSource = app.getBean(DataSource.class);
+        DataSource dataSource = (DataSource)app.getBean("dataSource");
+        Connection connection = dataSource.getConnection();
+
+        System.out.println(connection);
+        connection.close();
+    }
+
     @Test
     public void testDruid() throws SQLException {
         DruidDataSource druidDataSource = new DruidDataSource();
