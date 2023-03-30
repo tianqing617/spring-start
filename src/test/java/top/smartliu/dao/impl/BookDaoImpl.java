30c4a1b389aeda5f6fdd686b83346e4b915e5e20
Tianqing Liu <liutq@hollycrm.com>
2023-03-30 23:18:28 +0800

    Spring基本注解的三层构架

--- src/test/java/top/smartliu/dao/impl/BookDaoImpl.java
+++ src/test/java/top/smartliu/dao/impl/BookDaoImpl.java
@@ -1,7 +1,10 @@
 package top.smartliu.dao.impl;
 
+import org.springframework.stereotype.Repository;
 import top.smartliu.dao.BookDao;
 
+// <bean id="bookDao" class="top.smartliu.dao.impl.BookDaoImpl"/>
+@Repository("bookDao")
 public class BookDaoImpl implements BookDao {
     public void save() {
         System.out.println("book dao save ...");
--- src/test/java/top/smartliu/service/impl/BookServiceImpl.java
+++ src/test/java/top/smartliu/service/impl/BookServiceImpl.java
@@ -1,12 +1,23 @@
 package top.smartliu.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
 import top.smartliu.dao.BookDao;
 import top.smartliu.service.BookService;
 
+// <bean id="bookService" class="top.smartliu.service.impl.BookServiceImpl">
+@Service("bookService")
+@Scope("singleton")
 public class BookServiceImpl implements BookService {
+    @Autowired
+    @Qualifier("bookDao")
+//    @Resource(name="bookDao")
     private BookDao bookDao;
 
     public BookServiceImpl() {}
+
     public BookServiceImpl(BookDao bookDao) {
         this.bookDao = bookDao;
     }
