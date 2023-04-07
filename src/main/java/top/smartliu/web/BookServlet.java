074ee7ddd0d4ea65f8f57d465a138ddc69bf68e6
Tianqing Liu <liutq@hollycrm.com>
2023-04-07 16:03:19 +0800

    Spring集成Web

--- src/main/java/top/smartliu/web/BookServlet.java
+++ src/main/java/top/smartliu/web/BookServlet.java
@@ -2,6 +2,7 @@ package top.smartliu.web;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 import top.smartliu.service.BookService;
 
 import javax.servlet.*;
@@ -11,7 +12,12 @@ import java.io.IOException;
 public class BookServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
+        // 方式一：写死配置名
+        // ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // 方式二：动态获取
+        ServletContext servletContext = this.getServletContext();
+        ApplicationContext app = WebApplicationContextUtils.getWebApplicationContext(servletContext);
 
         BookService bookService = (BookService) app.getBean("bookService");
         bookService.save();
