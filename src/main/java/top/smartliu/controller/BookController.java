1df26caca00162773071d9848994d4ffe912c146
Tianqing Liu <liutq@hollycrm.com>
2023-04-10 11:28:43 +0800

    SpringMVC配置

---
+++ src/main/java/top/smartliu/controller/BookController.java
@@ -0,0 +1,21 @@
+package top.smartliu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@Controller
+//@RequestMapping("/book")
+public class BookController extends HttpServlet {
+    @RequestMapping("/quick")
+    public String save(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        System.out.println("Controller save running...");
+
+        return "success.jsp";
+    }
+}
---
+++ src/main/webapp/success.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: tianqing
+  Date: 2023/4/10
+  Time: 11:27
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Success</title>
+</head>
+<body>
+    <span>success</span>
+</body>
+</html>
