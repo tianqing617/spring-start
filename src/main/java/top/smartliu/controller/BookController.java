4ad8fd997d881d9286d857d1595488d61d69ce2a
Tianqing Liu <liutq@hollycrm.com>
2023-05-19 19:41:33 +0800

    获取请求参数：变量名和POJO映射

--- src/main/java/top/smartliu/controller/BookController.java
+++ src/main/java/top/smartliu/controller/BookController.java
@@ -3,6 +3,8 @@ package top.smartliu.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import top.smartliu.domain.User;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
@@ -14,6 +16,20 @@ public class BookController extends HttpServlet {
     @RequestMapping("/quick")
     public String save(HttpServletRequest request, HttpServletResponse response) throws IOException {
         System.out.println("Controller save running...");
+
+        return "success.jsp";
+    }
+    @RequestMapping("/quick2")
+    public String save2(String username, int age) throws IOException {
+        // /quick2?username=jim&age=18
+        System.out.println("save2" + username + age);
+
+        return "success.jsp";
+    }
+    @RequestMapping("/quick3")
+    public String save3(User user) throws IOException {
+        // /quick3?username=jim&age=18
+        System.out.println("save3" + user);
 
         return "success.jsp";
     }
