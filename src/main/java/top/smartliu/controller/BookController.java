a18bb9a8398962b06695720c09a37338a858b9f7
Tianqing Liu <liutq@hollycrm.com>
2023-05-21 20:17:58 +0800

    获取请求参数：数组和对象格式

--- src/main/java/top/smartliu/controller/BookController.java
+++ src/main/java/top/smartliu/controller/BookController.java
@@ -1,6 +1,8 @@
+//获取请求参数示例
 package top.smartliu.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,9 +12,11 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
-public class BookController extends HttpServlet {
+public class BookController {
     @RequestMapping("/quick")
     public String save(HttpServletRequest request, HttpServletResponse response) throws IOException {
         System.out.println("Controller save running...");
@@ -27,9 +31,23 @@ public class BookController extends HttpServlet {
         return "success.jsp";
     }
     @RequestMapping("/quick3")
-    public String save3(User user) throws IOException {
+    public String save3(@RequestBody User user) throws IOException {
         // /quick3?username=jim&age=18
         System.out.println("save3" + user);
+
+        return "success.jsp";
+    }
+    @RequestMapping("/quick4")
+    public String save4(String[] username) throws IOException {
+        // /quick4?username=jim&username=lucy
+        System.out.println("save4" + Arrays.asList(username));
+
+        return "success.jsp";
+    }
+    @RequestMapping("/quick5")
+    public String save5(@RequestBody List<User> userList) throws IOException {
+        // /quick5
+        System.out.println("save5" + userList);
 
         return "success.jsp";
     }
