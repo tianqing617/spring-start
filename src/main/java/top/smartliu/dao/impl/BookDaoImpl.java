76d2baa34b802d97d69f9fad261e4a134a0f3387
Tianqing Liu <liutq@hollycrm.com>
2023-05-31 17:10:12 +0800

    feat: JDBCTemplate配置示例

--- src/main/java/top/smartliu/dao/impl/BookDaoImpl.java
+++ src/main/java/top/smartliu/dao/impl/BookDaoImpl.java
@@ -1,9 +1,18 @@
 package top.smartliu.dao.impl;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
 import top.smartliu.dao.BookDao;
 
 public class BookDaoImpl implements BookDao {
     public void save() {
         System.out.println("book dao save ...");
+
+        // 查询数据库数据，并打印
+        ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
+        JdbcTemplate jdbcTemplate = app.getBean(JdbcTemplate.class);
+        int row = jdbcTemplate.update("insert into students values(?,?,?)", null, "lisi", 33);
+        System.out.println(row);
     }
 }
