8bfbbcb27788abc02f806c9040f2e0ee9b8ef804
Tianqing Liu <liutq@hollycrm.com>
2023-06-19 11:41:02 +0800

    feat: AOP增加环绕通知示例

--- src/main/java/top/smartliu/aop/MyAspect.java
+++ src/main/java/top/smartliu/aop/MyAspect.java
@@ -1,7 +1,19 @@
 package top.smartliu.aop;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+
 public class MyAspect {
     public void before(){
         System.out.println("前置增强..........");
+    }
+
+    // 环绕通知
+    public Object around(ProceedingJoinPoint pjp) throws Throwable {
+        System.out.println("环绕前增强");
+        // 切点方法
+        Object proceed = pjp.proceed();
+        System.out.println("环绕后增强");
+
+        return proceed;
     }
 }
