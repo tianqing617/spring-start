c5c36731a410aee45de751f073ac881f37604a41
Tianqing Liu <liutq@hollycrm.com>
2023-06-20 09:45:41 +0800

    feat: 注解方式实现AOP

--- src/main/java/top/smartliu/aop/MyAspect.java
+++ src/main/java/top/smartliu/aop/MyAspect.java
@@ -1,8 +1,14 @@
 package top.smartliu.aop;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
 
+@Component("myAspect")
+@Aspect
 public class MyAspect {
+    @Before("execution(public void top.smartliu.aop.Target.save())")
     public void before(){
         System.out.println("前置增强..........");
     }
--- src/main/java/top/smartliu/aop/Target.java
+++ src/main/java/top/smartliu/aop/Target.java
@@ -1,5 +1,8 @@
 package top.smartliu.aop;
 
+import org.springframework.stereotype.Component;
+
+@Component("target")
 public class Target implements TargetInterface {
     public void save() {
         System.out.println("Target save");
