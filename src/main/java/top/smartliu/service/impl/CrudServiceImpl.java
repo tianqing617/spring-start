4b9adaf2f6759a8eaa9dda9188f39407203ae0d1
Tianqing Liu <liutq@hollycrm.com>
2023-06-24 18:10:00 +0800

    feat: XML方式配置事务

--- src/main/java/top/smartliu/service/impl/CrudServiceImpl.java
+++ src/main/java/top/smartliu/service/impl/CrudServiceImpl.java
@@ -21,8 +21,8 @@ public class CrudServiceImpl implements CrudService {
     // 事务示例
     @Override
     public void txDemo() {
-        crudDao.updateDemo(3, 111);
-        int error = 1 / 0;
+        crudDao.updateDemo(3, 33);
+        // int error = 1 / 0;
         crudDao.updateDemo(4, 44);
     }
 }
