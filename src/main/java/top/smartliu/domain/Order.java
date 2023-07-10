39ed392021d392e2eca8f3cdab895878039f4b81
Tianqing Liu <liutq@hollycrm.com>
2023-07-10 14:14:20 +0800

    feat: 增加顾客和订单实体类

---
+++ src/main/java/top/smartliu/domain/Order.java
@@ -0,0 +1,50 @@
+package top.smartliu.domain;
+
+public class Order {
+    private  int id;
+    private String name;
+    private int total;
+    private Customer customer;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", total=" + total +
+                ", customer=" + customer +
+                '}';
+    }
+}
